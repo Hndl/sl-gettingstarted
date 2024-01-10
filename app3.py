e05478896ae7771788dda324845e48518582b4e9
Hndl.CGB <clive.bury@gmail.com>
2024-01-10 21:11:34 +0000

    Update app3.py

--- app3.py
+++ app3.py
@@ -9,7 +9,7 @@ st.title("Test App No.3" )
 # Create connection object and retrieve file contents.
 # Specify input format is a csv and to cache the result for 600 seconds.
 conn = st.connection('s3', type=FilesConnection)
-#df = conn.read("vw-ram-site-a/master-data/master.csv", "csv", ttl=600)
+df = conn.read("vw-ram-site-a/master-data/master.csv", input_format="csv", ttl=600)
 
 
 
