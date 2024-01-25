bd75b11ae1798a31d356a3f7a022e25b88cff93c
Hndl.CGB <clive.bury@gmail.com>
2024-01-25 14:14:05 +0000

    Update app4.py

--- app4.py
+++ app4.py
@@ -97,10 +97,18 @@ np.random.seed(seed=13)
 idx = pd.date_range("2023-07-11 10:05", "2023-07-13 23:00", freq='60min')
 wind_speeds = np.random.random_sample(size=len(idx)) * 15
 wind_directions = np.random.randint(0, high=3600, size=len(idx)) / 10.0
+dew_points = np.random.randint(-8, high=5, size=len(idx))
+amb_points = np.random.randint(-10, high=44, size=len(idx))
+
 dfW = pd.DataFrame({'datetime': idx, 'V_WIND': wind_speeds, 'DIR_WIND': wind_directions})
 dfW['datetime'] = pd.to_datetime(dfW.datetime)
 dfW = dfW.set_index('datetime')
 st.dataframe(dfW)
+
+dfT = pd.DataFrame({'datetime': idx, 'Ambient-Temperature': amb_points, 'Dew-Point-Temperature': dew_points})
+dfT['datetime'] = pd.to_datetime(dfT.datetime)
+dfT = dfT.set_index('datetime')
+st.dataframe(dfT)
 
 
 
